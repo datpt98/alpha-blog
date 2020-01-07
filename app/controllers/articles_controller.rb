c55529e15cf5fa0041d69b5e7a1a467861992f02
Dat Pham Tien <datpt98@gmail.com>
2020-01-08 01:03:13 +0200

    add partials + destroy options

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,14 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "Article was successfully destroyed"
+
+		redirect_to articles_path
+	end
+
 	def show
 		@article = Article.find(params[:id])	
 	end
