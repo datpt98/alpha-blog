f4b243db8efe7c3cf5abbb9b8011631727cb8248
Dat Pham Tien <datpt98@gmail.com>
2020-01-08 21:31:22 +0200

    Add stylings

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 	def index
 		@articles = Article.all
 	end
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def create
@@ -16,7 +17,7 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 
 		if @article.save
-			flash[:notice] = "Article was successfully created"
+			flash[:success] = "Article was successfully created"
 			redirect_to article_path(@article)
 		else
 			render 'new'
@@ -24,29 +25,31 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
-
 		if @article.update(article_params)
-			flash[:notice] = "Article was successfully updated"
+			flash[:success] = "Article was successfully updated"
 			redirect_to article_path(@article)
 		else
 			render 'edit'
 		end
 	end
 
+	def show
+	end
+
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
-		flash[:notice] = "Article was successfully destroyed"
+		flash[:danger] = "Article was successfully deleted"
 
 		redirect_to articles_path
 	end
 
-	def show
-		@article = Article.find(params[:id])	
-	end
 
 	private
+
+		def set_article
+			@article = Article.find(params[:id])
+		end
+
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
