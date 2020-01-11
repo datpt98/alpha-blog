e95bb47110f7272f06382844d18d0b0cb0a6b40a
Dat Pham Tien <datpt98@gmail.com>
2020-01-12 00:22:22 +0200

    auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :update, :show, :destroy]
 
 	def index
-		@articles = Article.all
+		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
@@ -45,7 +45,6 @@ class ArticlesController < ApplicationController
 
 
 	private
-
 		def set_article
 			@article = Article.find(params[:id])
 		end
