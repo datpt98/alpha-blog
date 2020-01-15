328975cf7dfad0155cf4a889de6ec6de5e33c551
Dat Pham Tien <datpt98@gmail.com>
2020-01-15 15:26:07 +0200

    update categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_225952) do
+ActiveRecord::Schema.define(version: 2020_01_15_122342) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
